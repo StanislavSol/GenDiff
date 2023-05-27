8e5c7d5b29005761bbcf614e3fcac566a2631220
StanislavSol <vandemar@bk.ru>
2023-05-27 14:56:08 +0300

    update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,6 +9,7 @@ def get_help():
                                                  'difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', metavar='FORMAT', help='set format of output')
     args = parser.parse_args()
     return args
 
