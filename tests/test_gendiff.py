1911a742ebe46e453757393bb2c9ae3a50e4e58b
StanislavSol <vandemar@bk.ru>
2023-05-28 14:23:22 +0300

    update

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,9 +2,9 @@ from gendiff.generators.file_comparison import generate_diff
 import json
 
 def get_files():
-    path_file1 = '/home/ssol/python-project-50/test/fixtures/file1.json'
-    path_file2 = '/home/ssol/python-project-50/test/fixtures/file2.json'
-    path_sample_file = '/home/ssol/python-project-50/test/fixtures/sample_output.json'
+    path_file1 = '/home/ssol/python-project-50/tests/fixtures/file1.json'
+    path_file2 = '/home/ssol/python-project-50/tests/fixtures/file2.json'
+    path_sample_file = '/home/ssol/python-project-50/tests/fixtures/sample_output.json'
     with open(path_sample_file, 'r', encoding='utf=8') as sample:
         return path_file1, path_file2, sample.read()
 
